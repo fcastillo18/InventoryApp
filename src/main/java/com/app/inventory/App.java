c9bee4f2ee8d97289fac2f6c487a9e253d70551c
Franklin Castillo <franklin_castillo@claro.com.do>
2018-04-10 16:43:43 -0400

    2dn commit

---
+++ src/main/java/com/app/inventory/App.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.app.inventory;
+
+import com.app.inventory.dao.ConnectionDB;
+import com.app.inventory.dao.SystemDaoJdbc;
+import com.app.inventory.view.MainForm;
+
+/**
+ *
+ * @author Franklin Castillo
+ */
+public class App {
+    public static void main(String[] args) {
+        MainForm mainForm = new MainForm();
+        //mainForm.setVisible(true);
+        new ConnectionDB().startServer();
+        SystemDaoJdbc daoJdbc = new SystemDaoJdbc();
+        daoJdbc.execTask();
+        
+    }
+}
